7b3f829aa94f81c1fb3719b140d0b1fcd7e6e5ed
Brittany Morris <brittmmendez@gmail.com>
2018-03-12 19:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
